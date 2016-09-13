b82ef2ce5e2be5deac7706b7daff76ed7cff1f1e
Jason Sisk <sisk@bellycard.com>
2016-09-13 13:15:21 -0500

    Add comments about how this deviates from mainline

--- lib/active_zuora/generator.rb
+++ lib/active_zuora/generator.rb
@@ -197,6 +197,9 @@ module ActiveZuora
         include LazyAttr
         exclude_from_queries :overage_price, :included_units,
           :discount_amount, :discount_percentage, :rollover_balance
+        # as of 2016-09-13, this deviates from the main gem repositoiry
+        # at https://github.com/sportngin/active_zuora
+        # it has been updated to remove lazy loading of :price
       end
 
       customize 'Refund' do
